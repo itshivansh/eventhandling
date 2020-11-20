34c47dde67414dcf5c3af0c19628f0e8c2d6207a
shivansh <shivanshakb@gmail.com>
2020-11-20 11:24:35 +0530

    todoApp

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -10,7 +10,9 @@ import { Todo } from '../models/Todo';
 
 export class TodoComponent implements OnInit {
   // todoList contains all todos
-  todoList: Todo[];
+  todoList: Todo[]=[];
+  public clr:boolean;
+  public status:string = "Undone"; 
   constructor() { }
 
   ngOnInit() {
@@ -18,17 +20,38 @@ export class TodoComponent implements OnInit {
   // write logic to the onAddTodo method is to add a new todo to list
   // get maximum id in list and assign maximum id plus one while adding a todo
   onAddTodo(todoText: any) {
+    if(todoText.length>0&&todoText !==''){
+      this.clr=false;
+      let max = this.todoList.length;
+      var todoarr={
+        todoId:max+1,
+        text:todoText,
+        isCompleted:false
+      }
+      this.todoList.push(todoarr);
+      console.log(this.todoList)
+    }
   }
 
   // write logic to the onClearTodos method to delete the all todos in the todoList
   onClearTodos() {
+    this.clr=true;
+    this.todoList=[];
   }
 
   // write logic to method onCompletingTask, to mark todo as as completed or not
   onCompletingTodo(todo: Todo) {
+    if(todo.isCompleted){
+      todo.isCompleted=false;
+    }
+    else{
+      todo.isCompleted=true;
+    }
   }
 
   // write logic to method onDeletingTask, to delete the todo
   onDeletingTodo(todoId) {
+     this.todoList=this.todoList.filter(t=>t.todoId !==todoId)
+    //this.todoList=this.todoList.filter(todoList=>todoList.todoId !==todoList.todoId)
   }
 }
